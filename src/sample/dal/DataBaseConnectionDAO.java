04cb376b69175582fc88779a6eddd4f3e8420ac6
Agus0136 <71644405+Agus0136@users.noreply.github.com>
2021-04-11 02:34:50 +0200

    Update DataBaseConnectionDAO.java

--- src/sample/dal/DataBaseConnectionDAO.java
+++ src/sample/dal/DataBaseConnectionDAO.java
@@ -7,25 +7,33 @@ import sample.utils.config.DBConfigReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class DataBaseConnectionDAO {
 
-    private DBConfigReader config;
-    private SQLServerDataSource dataSource;
+    private static SQLServerDataSource dataSource;
 
     public DataBaseConnectionDAO() {
-        try {
-            config = new DBConfigReader("db.properties");
-            dataSource = new SQLServerDataSource();
 
-            dataSource.setServerName(config.getIP());
-            dataSource.setDatabaseName(config.getDBName());
-            dataSource.setUser(config.getUsername());
-            dataSource.setPassword(config.getPassword());
-            dataSource.setPortNumber(1433);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName( "10.176.111.31" );
+        dataSource.setDatabaseName( "Compulsory_Assignment_2" );
+        dataSource.setUser( "CSe20B_7" );
+        dataSource.setPassword( "CSe20B_7" );
+        dataSource.setPortNumber( 1433 );
+    }
+
+    public static Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+
+    public static void main(String[] args) throws SQLException {
+
+        DataBaseConnectionDAO dataBaseConnectionDAO = new DataBaseConnectionDAO();
+        Connection connection = dataBaseConnectionDAO.getConnection();
+
+        System.out.println( "Is it Open?" + !connection.isClosed() );
+
+        connection.close();
     }
 }
