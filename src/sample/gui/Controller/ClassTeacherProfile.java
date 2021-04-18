87284a60b29ff6b7fc0c8f2a1da3fd8d91f140de
Agus0136 <71644405+Agus0136@users.noreply.github.com>
2021-04-18 18:41:47 +0200

    Update ClassTeacherProfile.java

--- src/sample/gui/Controller/ClassTeacherProfile.java
+++ src/sample/gui/Controller/ClassTeacherProfile.java
@@ -1,4 +1,38 @@
 package sample.gui.Controller;
 
-public class ClassTeacherProfile {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClassTeacherProfile implements Initializable {
+
+    public Hyperlink btnLogOut;
+
+    public ClassTeacherProfile() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void Logout(ActionEvent actionEvent) throws IOException {
+
+        Stage currentStage = (Stage) btnLogOut.getScene().getWindow();
+
+        Stage loginStage = new Stage();
+        Parent root = FXMLLoader.load( getClass().getResource( "/Sample/gui/View/MainWindow.fxml" ) );
+        loginStage.setScene( new Scene( root ) );
+        loginStage.show();
+        currentStage.close();
+    }
 }
